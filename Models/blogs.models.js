e242feec367363a516f61198a85b8657940193ec
Viraj Gavade <vrajgavade17@gmail.com>
2024-09-26 07:29:10 +0530

    Created the bog schema

---
+++ Models/blogs.models.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+
+
+const BlogSchema = new mongoose.Schema({
+
+    title:{
+        type:String,
+        required:true
+    },
+    body:{
+        type:String,
+        required:true
+    },
+
+    CoverImageURL:{
+        type:String,
+        required:false,
+        default:""
+    },
+
+    createdBy:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User"
+    }
+
+},{
+    timestamps:true
+})
+
+module.exports = mongoose.model('Blog',BlogSchema)
