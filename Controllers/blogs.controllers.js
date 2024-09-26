d8a37e43a20e45df9272b1843dc58bde519fd93f
Viraj Gavade <vrajgavade17@gmail.com>
2024-09-26 08:16:41 +0530

    "Added blog posting functionality with image upload and updated nav link to point to new blog endpoint"

--- Controllers/blogs.controllers.js
+++ Controllers/blogs.controllers.js
@@ -1,1 +1,30 @@
+const upload = require('../Middlewares/multer.middleware')
 const Blog = require('../Models/blogs.models')
+const uploadFile = require('../Utils/cloudinary')
+
+const PostBlog = async (req,res)=>{
+    const { title , body } = req.body
+
+    const CoverImageLocalpath = req.file.path
+    if(!CoverImageLocalpath){
+        throw new Error('Cover image localpath not found!')
+    }
+
+    const CoverImageURL = await uploadFile(CoverImageLocalpath)
+    if(!CoverImageURL.url){
+        throw new Error('Something went wrong while uploading file on cloudinary!')
+
+    }
+    const blog = await Blog.create({
+        title:title,
+        body:body,
+        CoverImageURL:CoverImageURL,
+        createdBy:req.user
+    })
+
+    return res.render('home')
+}
+
+module.exports = {
+    PostBlog
+}
