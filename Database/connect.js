10cfe6f1a633ff772404a429c9d68efb600f42dc
Viraj Gavade <vrajgavade17@gmail.com>
2024-11-09 18:44:55 +0530

    "Removed db_url parameter from connectDB function and added await keyword to mongoose.connect call"

--- Database/connect.js
+++ Database/connect.js
@@ -1,10 +1,9 @@
 require('dotenv').config()
 const mongoose = require('mongoose')
 
-
-const connectDB = async (db_url) =>{
+const connectDB = async () =>{
     try {
-        const connect = mongoose.connect(
+        const connect = await mongoose.connect(
             process.env.MONGO_URI,{
                 useNewUrlParser: true,
             // useCreateIndex: true,
